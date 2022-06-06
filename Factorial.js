cfa29e7188210b8bb5d751d60fa1a17c0d6f88b6
manja1227 <manjunatha1227@gmail.com>
2022-06-06 23:47:57 +0530

    recursive factorial solution

--- Factorial.js
+++ Factorial.js
@@ -1,12 +1,30 @@
 function factorial(num) {
-    var factorial = 1;
+    let factorial = 1;
     if (num > 0) {
-        for (var i = num; i > 1; i--) {
+        for (let i = num; i > 1; i--) {
             factorial *= i;
         }
     }
     return factorial;
 }
+console.time();
 console.log(factorial(0));
 console.log(factorial(1));
 console.log(factorial(5));
+console.log(factorial(10));
+console.log(factorial(121));
+console.log(factorial(155));
+console.timeEnd();
+function recursiveFactorial(num) {
+    if (num === 0)
+        return 1;
+    return num * recursiveFactorial(num - 1);
+}
+console.time();
+console.log(recursiveFactorial(0));
+console.log(recursiveFactorial(1));
+console.log(recursiveFactorial(5));
+console.log(recursiveFactorial(10));
+console.log(recursiveFactorial(121));
+console.log(recursiveFactorial(155));
+console.timeEnd();
--- Factorial.ts
+++ Factorial.ts
@@ -8,6 +8,25 @@ function factorial(num: number) {
   return factorial;
 }
 
+console.time();
 console.log(factorial(0));
 console.log(factorial(1));
 console.log(factorial(5));
+console.log(factorial(10));
+console.log(factorial(121));
+console.log(factorial(155));
+console.timeEnd();
+
+function recursiveFactorial(num: number) {
+  if (num === 0) return 1;
+  return num * recursiveFactorial(num - 1);
+}
+
+console.time();
+console.log(recursiveFactorial(0));
+console.log(recursiveFactorial(1));
+console.log(recursiveFactorial(5));
+console.log(recursiveFactorial(10));
+console.log(recursiveFactorial(121));
+console.log(recursiveFactorial(155));
+console.timeEnd();
