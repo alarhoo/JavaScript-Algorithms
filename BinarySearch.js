f1240123f4e57c169d53c1acd2f3ba76bb340405
manja1227 <manjunatha1227@gmail.com>
2022-06-07 23:46:36 +0530

    recursive binary search solution

--- BinarySearch.js
+++ BinarySearch.js
@@ -3,7 +3,7 @@ function BinarySearch(numbers, target) {
     let rightIndex = numbers.length - 1;
     while (leftIndex <= rightIndex) {
         let midIndex = Math.floor((leftIndex + rightIndex) / 2);
-        if (target == numbers[midIndex]) {
+        if (target === numbers[midIndex]) {
             return midIndex;
         }
         if (target < numbers[midIndex]) {
@@ -19,4 +19,27 @@ console.time();
 console.log(BinarySearch([-5, 2, 4, 6, 10], 10));
 console.log(BinarySearch([-5, 2, 4, 6, 10], 6));
 console.log(BinarySearch([-5, 2, 4, 6, 10], 20));
+console.timeEnd();
+function recursiveBinarySearch(numbers, target) {
+    return search(numbers, target, 0, numbers.length - 1);
+}
+function search(arr, target, leftIndex, rightIndex) {
+    if (leftIndex > rightIndex) {
+        return -1;
+    }
+    let midIndex = Math.floor((leftIndex + rightIndex) / 2);
+    if (target === arr[midIndex]) {
+        return midIndex;
+    }
+    if (target < arr[midIndex]) {
+        return search(arr, target, leftIndex, midIndex - 1);
+    }
+    else {
+        return search(arr, target, midIndex + 1, rightIndex);
+    }
+}
+console.time();
+console.log(recursiveBinarySearch([-5, 2, 4, 6, 10], 10));
+console.log(recursiveBinarySearch([-5, 2, 4, 6, 10], 6));
+console.log(recursiveBinarySearch([-5, 2, 4, 6, 10], 20));
 console.timeEnd();
--- BinarySearch.ts
+++ BinarySearch.ts
@@ -4,7 +4,7 @@ function BinarySearch(numbers: number[], target: number) {
 
   while (leftIndex <= rightIndex) {
     let midIndex = Math.floor((leftIndex + rightIndex) / 2);
-    if (target == numbers[midIndex]) {
+    if (target === numbers[midIndex]) {
       return midIndex;
     }
     if (target < numbers[midIndex]) {
@@ -21,4 +21,29 @@ console.time();
 console.log(BinarySearch([-5, 2, 4, 6, 10], 10));
 console.log(BinarySearch([-5, 2, 4, 6, 10], 6));
 console.log(BinarySearch([-5, 2, 4, 6, 10], 20));
+console.timeEnd();
+
+function recursiveBinarySearch(numbers: number[], target: number) {
+  return search(numbers, target, 0, numbers.length - 1);
+}
+
+function search(arr, target, leftIndex, rightIndex) {
+  if (leftIndex > rightIndex) {
+    return -1;
+  }
+  let midIndex = Math.floor((leftIndex + rightIndex) / 2);
+  if (target === arr[midIndex]) {
+    return midIndex;
+  }
+  if (target < arr[midIndex]) {
+    return search(arr, target, leftIndex, midIndex - 1);
+  } else {
+    return search(arr, target, midIndex + 1, rightIndex);
+  }
+}
+
+console.time();
+console.log(recursiveBinarySearch([-5, 2, 4, 6, 10], 10));
+console.log(recursiveBinarySearch([-5, 2, 4, 6, 10], 6));
+console.log(recursiveBinarySearch([-5, 2, 4, 6, 10], 20));
 console.timeEnd();
